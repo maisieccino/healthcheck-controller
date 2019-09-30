87bb3dfd0fc5a96b117577a73a2229ee00fb7769
Matt Bell <matt@mbell.me>
2019-09-30 15:13:23 +0100

    Fix codegen script

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -28,7 +28,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/mbellgb/healthcheck-controller/pkg/generated github.com/mbellgb/healthcheck-controller/pkg/apis \
   healthcheck:v1alpha1 \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
 # To use your own boilerplate text append:
